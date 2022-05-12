1b6181ee287acc97451cc4d536e2d5e44f84ad8e
syedashar1 <saghirashar@gmail.com>
2022-05-12 15:43:39 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import sanityClient from "./client.js";
 import './App.css';
 
 function App() {
+
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    sanityClient
+    .fetch(
+      `*[_type == "movie"]{
+        title,
+        slug,
+        overview,
+        releaseDate,
+        popularity,
+        poster,
+        castMembers
+    }`
+    )
+    .then((data) => console.log(data))
+  }, [ ])
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
---
+++ src/client.js
@@ -0,0 +1,6 @@
+import sanityClient from '@sanity/client'
+
+export default sanityClient({
+    projectId: "mpf2uu0i",
+    dataset: "production"
+})
